fc29744b6925ba916acd6e915fd83ecde0d9c162
Gerardo <gerardogcordero@outlook.com>
2022-04-25 23:38:29 -0500

    fix variable type

--- app/Interfaces/FuelStationInterface.php
+++ app/Interfaces/FuelStationInterface.php
@@ -8,7 +8,7 @@ interface FuelStationInterface
 {
     public function all();
     public function show(FuelStation $fuelStation);
-    public function create(array $data);
-    public function update(FuelStation $fuelStation, array $data);
+    public function create($data);
+    public function update(FuelStation $fuelStation, $data);
     public function delete(FuelStation $fuelStation);
 }
