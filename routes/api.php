fee8d4a176070f0f4ccdb116092b671cc9e5c940
Gerardo <gerardogcordero@outlook.com>
2022-04-25 19:47:45 -0500

    route for fuel stations resource added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\FuelStationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::apiResource('fuel-stations', FuelStationController::class)
+        ->parameter('fuel-stations', 'fuelStation');
 });
